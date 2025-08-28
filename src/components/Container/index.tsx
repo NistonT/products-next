9ccd4ceb006ca33c43d1662e63eb1ae23bf20b41
NistonN <nisprogrammer@gmail.com>
2025-08-29 00:27:15 +0300

    fix

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,5 +1,7 @@
 import type { PropsWithChildren } from "react";
 
+// Контейнер
+
 export const Container = ({ children }: PropsWithChildren) => {
   return <div className="max-w-(--my-max-width) mx-auto px-2">{children}</div>;
 };

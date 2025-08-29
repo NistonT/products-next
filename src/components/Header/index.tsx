936d47024176a3d17e831a95448bb48253941d36
NistonN <nisprogrammer@gmail.com>
2025-08-29 13:49:16 +0300

    fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { Heart, Search, ShoppingCart, Sun } from "lucide-react";
-import { Button } from "../ui/button";
-import { Input } from "../ui/input";
+import { Button } from "./../ui/button";
+import { Input } from "./../ui/input";
 import { Logo } from "./Logo";
 import { Navigation } from "./Navigation";
 
